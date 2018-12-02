daa4fd83e3bd26cc6e016e434f1f52fad7022372
Alekssenko <sasha18081960@gmail.com>
2018-12-02 11:31:36 +0300

    first commit

---
+++ jaguar.js
@@ -0,0 +1,16 @@
+
+    $(document).ready(function () {
+        $('.about-us-slider').slick({
+            arrows: false,
+            dots: true
+        });
+        $('#burger').click(function () {
+            $('.header ul').toggleClass('show');
+                           });
+        $(window).resize(function () {
+            if ($(window).width() >= 700) {
+                $('.header ul').removeClass('show');
+
+            }
+        })
+    })
